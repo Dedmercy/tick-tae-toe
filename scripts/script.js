114361b6a6f4f3f024ee7bbfef532afe681bcb9b
Dedmercy <Dedmercy@yandex.ru>
2022-10-14 05:26:12 +0300

    1.0.5

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
+let state_array = []
+let selectedCharacter = "cross"
 
 
+for (let i = 0; i < 9; i++){
+    state_array.push("")
+}
+
+function selectZero(){
+    selectedCharacter = "zero"
+
+}
+
+function selectCross(){
+    selectedCharacter = "cross"
+}
 
 
 function clickEvent(canvas) {
-   canvas.classList.add("zero")
+    if(canvas)
+    console.log(canvas.classList)
 }
+
